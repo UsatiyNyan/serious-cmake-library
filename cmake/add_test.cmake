75df1c8e42817be9208e3645c23c7c66b735bf98
Usatiy Nyan <kirillarionov123456@gmail.com>
2023-08-18 20:37:53 +0200

    cmake/add_test: gtest_prologue and gtest->ctest discoverability

--- cmake/add_test.cmake
+++ cmake/add_test.cmake
@@ -1,7 +1,14 @@
 include(cmake/system_link.cmake)
+include(cmake/CPM.cmake)
+
+macro(sl_gtest_prologue version)
+    cpmaddpackage("gh:google/googletest#${version}")
+    include(GoogleTest)
+endmacro()
 
 function(sl_add_gtest lib name)
     add_executable(${lib}_${name}_test src/${name}_test.cpp)
     target_link_libraries(${lib}_${name}_test PRIVATE ${lib})
     sl_target_link_system_library(${lib}_${name}_test PRIVATE gtest_main)
+    gtest_discover_tests(${lib}_${name}_test)
 endfunction()
