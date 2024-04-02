a841fbe892881aa596480fdf14f138d260864142
Usatiy Nyan <kirillarionov123456@gmail.com>
2024-04-02 22:07:20 +0200

    collectable targets for examples and tests

--- cmake/add_example.cmake
+++ cmake/add_example.cmake
@@ -2,4 +2,10 @@ function(sl_add_example lib name)
     set(example_executable ${lib}_${name})
     add_executable(${example_executable} src/${name}.cpp)
     target_link_libraries(${example_executable} PRIVATE ${lib})
+
+    if (NOT TARGET ${lib}_examples)
+        add_custom_target(${lib}_examples)
+    endif()
+
+    add_dependencies(${lib}_examples ${example_executable})
 endfunction()
--- cmake/add_test.cmake
+++ cmake/add_test.cmake
@@ -7,8 +7,15 @@ macro(sl_gtest_prologue version)
 endmacro()
 
 function(sl_add_gtest lib name)
-    add_executable(${lib}_${name}_test src/${name}_test.cpp)
+    set(test_executable ${lib}_${name}_test)
+    add_executable(${test_executable} src/${name}_test.cpp)
     target_link_libraries(${lib}_${name}_test PRIVATE ${lib})
     sl_target_link_system_library(${lib}_${name}_test PRIVATE gtest_main)
     gtest_discover_tests(${lib}_${name}_test)
+
+    if (NOT TARGET ${lib}_tests)
+        add_custom_target(${lib}_tests)
+    endif()
+
+    add_dependencies(${lib}_tests ${test_executable})
 endfunction()
